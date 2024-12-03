95ebf46a336fcb7df4e3de26178797a1f24571ee
Pitius <82815028+droidpeti@users.noreply.github.com>
2024-12-03 09:46:22 +0100

    Update SeedData.cs
    Fixed Seeder

--- MVCHulladek/Models/SeedData.cs
+++ MVCHulladek/Models/SeedData.cs
@@ -1,16 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MVCHulladek.Data;
 
 namespace MVCHulladek.Models
 {
     public class SeedData
     {
-        
         public static void EnsurePopulated(IApplicationBuilder app)
         {
-            
             MVCHulladekContext context = app.ApplicationServices
-            .CreateScope().ServiceProvider.GetRequiredService<MVCHulladekContext>();
+                .CreateScope().ServiceProvider.GetRequiredService<MVCHulladekContext>();
 
             if (context.Database.GetPendingMigrations().Any())
             {
@@ -18,11 +16,6 @@ namespace MVCHulladek.Models
             }
             if (!context.Szolgaltatas.Any())
             {
-                foreach (Naptar naptar in context.Naptar)
-                    context.Naptar.Remove(naptar);
-                foreach (Lakig lakig in context.Lakig)
-                    context.Lakig.Remove(lakig);
-
                 context.Szolgaltatas.AddRange(
                     new Szolgaltatas
                     {
@@ -46,56 +39,34 @@ namespace MVCHulladek.Models
                     }
                 );
                 context.SaveChanges();
-                context.Naptar.AddRange(
-                    new Naptar { Datum = new DateTime(2020, 01, 15), SzolgaltatasId = 1 },
-                    new Naptar { Datum = new DateTime(2020, 02, 16), SzolgaltatasId = 2 },
-                    new Naptar { Datum = new DateTime(2020, 03, 17), SzolgaltatasId = 3 },
-                    new Naptar { Datum = new DateTime(2020, 04, 18), SzolgaltatasId = 4 },
-                    new Naptar { Datum = new DateTime(2020, 05, 19), SzolgaltatasId = 5 },
-                    new Naptar { Datum = new DateTime(2020, 06, 20), SzolgaltatasId = 6 },
-                    new Naptar { Datum = new DateTime(2020, 07, 21), SzolgaltatasId = 7 },
-                    new Naptar { Datum = new DateTime(2020, 08, 22), SzolgaltatasId = 8 },
-                    new Naptar { Datum = new DateTime(2020, 09, 23), SzolgaltatasId = 9 },
-                    new Naptar { Datum = new DateTime(2020, 10, 24), SzolgaltatasId = 10 },
-                    new Naptar { Datum = new DateTime(2020, 11, 25), SzolgaltatasId = 11 },
-                    new Naptar { Datum = new DateTime(2020, 12, 26), SzolgaltatasId = 12 },
-                    new Naptar { Datum = new DateTime(2021, 01, 27), SzolgaltatasId = 13 },
-                    new Naptar { Datum = new DateTime(2021, 02, 28), SzolgaltatasId = 14 },
-                    new Naptar { Datum = new DateTime(2021, 03, 29), SzolgaltatasId = 15 },
-                    new Naptar { Datum = new DateTime(2021, 04, 30), SzolgaltatasId = 16 },
-                    new Naptar { Datum = new DateTime(2021, 05, 31), SzolgaltatasId = 17 },
-                    new Naptar { Datum = new DateTime(2021, 06, 01), SzolgaltatasId = 18 },
-                    new Naptar { Datum = new DateTime(2021, 07, 02), SzolgaltatasId = 19 },
-                    new Naptar { Datum = new DateTime(2021, 08, 03), SzolgaltatasId = 20 }
-                );
-                context.SaveChanges();
 
-                context.Lakig.AddRange(
-                    new Lakig { Igeny = new DateTime(2020, 01, 15), SzolgaltatasId = 1, Mennyiseg = 1 },
-                    new Lakig { Igeny = new DateTime(2020, 02, 16), SzolgaltatasId = 2, Mennyiseg = 2 },
-                    new Lakig { Igeny = new DateTime(2020, 03, 17), SzolgaltatasId = 3, Mennyiseg = 3 },
-                    new Lakig { Igeny = new DateTime(2020, 04, 18), SzolgaltatasId = 4, Mennyiseg = 4 },
-                    new Lakig { Igeny = new DateTime(2020, 05, 19), SzolgaltatasId = 5, Mennyiseg = 5 },
-                    new Lakig { Igeny = new DateTime(2020, 06, 20), SzolgaltatasId = 6, Mennyiseg = 6 },
-                    new Lakig { Igeny = new DateTime(2020, 07, 21), SzolgaltatasId = 7, Mennyiseg = 7 },
-                    new Lakig { Igeny = new DateTime(2020, 08, 22), SzolgaltatasId = 8, Mennyiseg = 8 },
-                    new Lakig { Igeny = new DateTime(2020, 09, 23), SzolgaltatasId = 9, Mennyiseg = 9 },
-                    new Lakig { Igeny = new DateTime(2020, 10, 24), SzolgaltatasId = 10, Mennyiseg = 10 },
-                    new Lakig { Igeny = new DateTime(2020, 11, 25), SzolgaltatasId = 11, Mennyiseg = 11 },
-                    new Lakig { Igeny = new DateTime(2020, 12, 26), SzolgaltatasId = 12, Mennyiseg = 12 },
-                    new Lakig { Igeny = new DateTime(2021, 01, 27), SzolgaltatasId = 13, Mennyiseg = 13 },
-                    new Lakig { Igeny = new DateTime(2021, 02, 28), SzolgaltatasId = 14, Mennyiseg = 14 },
-                    new Lakig { Igeny = new DateTime(2021, 03, 29), SzolgaltatasId = 15, Mennyiseg = 15 },
-                    new Lakig { Igeny = new DateTime(2021, 04, 30), SzolgaltatasId = 16, Mennyiseg = 16 },
-                    new Lakig { Igeny = new DateTime(2021, 05, 31), SzolgaltatasId = 17, Mennyiseg = 17 },
-                    new Lakig { Igeny = new DateTime(2021, 06, 01), SzolgaltatasId = 18, Mennyiseg = 18 },
-                    new Lakig { Igeny = new DateTime(2021, 07, 02), SzolgaltatasId = 19, Mennyiseg = 19 },
-                    new Lakig { Igeny = new DateTime(2021, 08, 03), SzolgaltatasId = 20, Mennyiseg = 20 }
-                );
+                var szolgaltatasList = context.Szolgaltatas.ToList();
+
+                var random = new Random();
+                var naptarList = new List<Naptar>();
+                var lakigList = new List<Lakig>();
+
+                for (int i = 1; i <= 100; i++)
+                {
+                    var szolgaltatasId = szolgaltatasList[random.Next(szolgaltatasList.Count)].SzolgaltatasId;
+                    naptarList.Add(new Naptar
+                    {
+                        Datum = DateTime.Now.AddDays(random.Next(-1000, 1000)),
+                        SzolgaltatasId = szolgaltatasId
+                    });
+
+                    lakigList.Add(new Lakig
+                    {
+                        Igeny = DateTime.Now.AddDays(random.Next(-1000, 1000)),
+                        SzolgaltatasId = szolgaltatasId,
+                        Mennyiseg = random.Next(1, 21)
+                    });
+                }
+
+                context.Naptar.AddRange(naptarList);
+                context.Lakig.AddRange(lakigList);
                 context.SaveChanges();
-                
             }
-            
         }
     }
 }
